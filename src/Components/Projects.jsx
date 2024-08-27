f612532a2db2af6f91bb51572c5f7bc97310b512
abdulrahmanmundhera3637 <abdulrahmanmundhera@gmail.com>
2024-08-26 20:08:55 -0700

    Update project.js

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,6 +3,7 @@ import project1 from "../assets/project1.png";
 import project2 from "../assets/project2.png";
 import project3 from "../assets/project3.png";
 import project4 from "../assets/project4.png";
+import project5 from "../assets/project5.png";
 
 
 const projects = [
