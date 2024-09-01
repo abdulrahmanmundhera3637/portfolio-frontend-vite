e4845d7785b608313444899c8bab18e1cdf5c756
abdulrahmanmundhera3637 <abdulrahmanmundhera@gmail.com>
2024-08-31 22:42:06 -0700

    CV Changed

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,7 +3,7 @@ import heroImg from "../assets/me.jpg";
 
 const Hero = () => {
   const phoneNumber = "+923043637810";
-  const resumeLink = "/Resume.pdf"; 
+  const resumeLink = "/Abdul-Rahman-CV.pdf"; 
 
   return (
     <div className='bg-black text-white text-center py-4 md:py-12'>
